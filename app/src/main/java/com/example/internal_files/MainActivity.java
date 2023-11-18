b6c88f5c84b0dbd03da7c016a51a10168c06d65e
roeys <roeys1234@gmail.com>
2023-11-18 17:19:22 +0200

    init

--- app/src/main/java/com/example/internal_files/MainActivity.java
+++ app/src/main/java/com/example/internal_files/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.internal_files;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -16,20 +19,37 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class MainActivity extends AppCompatActivity {
-    private final String FILENAME = "inttext.txt";
+    /**
+     * @author Roey Schwartz rs7532@bs.amalnet.k12.il
+     * @version 1
+     * @since 18.11.2023
+     * this code will show an EditText and you can save to a file what you wrote, reset the content of the file and
+       close the application with that saves the current content of the file
+     */
+    private final String FILENAME = "intText.txt";
     EditText et;
+    TextView tv;
 
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tv = findViewById(R.id.tv);
         et = findViewById(R.id.et);
 
-        et.setText(Read_text());
+        tv.setText(Read_text());
 
     }
+
+    /**
+     * <p>
+     *     the function get a text that the user wants to save in the file,
+     *     the function will add that text to the current content in the file
+     * </>
+     */
 
     public void Write_text(String text){
         try {
@@ -45,17 +65,25 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * <p>
+     *     the function doesn't get an variable,
+     *     the function will return a text type of String that including the current content in the file
+     * </>
+     */
+
     public String Read_text(){
         FileInputStream fIS = null;
         try {
             fIS = openFileInput(FILENAME);
         } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+            Write_text("");
         }
+
         InputStreamReader iSR = new InputStreamReader(fIS);
         BufferedReader bR = new BufferedReader(iSR);
         StringBuilder sB = new StringBuilder();
-        String line = null;
+        String line;
         try {
             line = bR.readLine();
             while(line != null){
@@ -67,26 +95,60 @@ public class MainActivity extends AppCompatActivity {
             iSR.close();
             fIS.close();
             return String.valueOf(sB);
-    } catch (IOException e) {
+    } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
+
+    /**
+     * <p>
+     *     the function get a variable of View type,
+     *     the function will save the text that the user wants to save and show it.
+     * </>
+     */
     public void savePressed(View view) {
-            // READ:
-            String sB = Read_text();
-            // WRITE:
-            Write_text(String.valueOf(sB));
+        // READ:
+        System.out.println("haven't got the massage!");
+        String fileString = Read_text();
+        System.out.println("got the massage!");
+        // WRITE:
+        Write_text(String.valueOf(fileString));
+        tv.setText(fileString);
     }
 
+    /**
+     * <p>
+     *     the function get a variable of View type,
+     *     the function will delete the content of the file
+     * </>
+     */
     public void resetPressed(View view) {
         Write_text("");
+        tv.setText("");
     }
 
+    /**
+     * <p>
+     *     the function get a variable of View type,
+     *     the function will save the content in the EditText to the file and the current content in the file and close the application
+     * </>
+     */
     public void exitPressed(View view) {
         // READ:
         String sB = Read_text();
         // WRITE:
         Write_text(String.valueOf(sB));
         finish();
+    }
+
+    /**
+     * <p>
+     *     the function get a variable of View type,
+     *     the function will transfer the user to the credits screen
+     * </>
+     */
+    public void creditsPressed(View view) {
+        Intent si = new Intent(this, credits.class);
+        startActivity(si);
     }
 }
